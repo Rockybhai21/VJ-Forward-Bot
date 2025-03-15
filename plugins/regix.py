2d8fca131418bc383e2a4c29d83a888f4baaa3bd
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-03-15 15:29:40 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -24,6 +24,13 @@ from pyrogram.types import Message
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+def clean_text(text):
+    """Removes all links, Telegram links, and @mentions from the given text."""
+    text = re.sub(r'https?://\S+', '', text)  # Remove all links
+    text = re.sub(r't\.me/\S+', '', text)  # Remove Telegram links
+    text = re.sub(r'@\w+', '', text)  # Remove @mentions
+    return text.strip()
+    
 CLIENT = CLIENT()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -33,12 +40,7 @@ TEXT = Script.TEXT
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-def clean_text(text):
-    """Removes all links, Telegram links, and @mentions from the given text."""
-    text = re.sub(r'https?://\S+', '', text)  # Remove all links
-    text = re.sub(r't\.me/\S+', '', text)  # Remove Telegram links
-    text = re.sub(r'@\w+', '', text)  # Remove @mentions
-    return text.strip()
+
 
 @Client.on_callback_query(filters.regex(r'^start_public'))
 async def pub_(bot, message):
